57788edbaed9d34e988803880bd252b4dd62e96e
Aziz Patel <ac98844@avcol.school.nz>
2021-02-19 14:37:10 +1300

    Creating the route for the About Us Page

--- Automotive Repair WebApp/Controllers/HomeController.cs
+++ Automotive Repair WebApp/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Automotive_Repair_WebApp.Controllers
             return View();
         }
 
+        public IActionResult Aboutus()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
