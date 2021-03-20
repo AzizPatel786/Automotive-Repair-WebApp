07aac68e0e4bd6539e57aae5bc28970d0ea87181
Aziz Patel <ac98844@avcol.school.nz>
2021-03-21 12:52:53 +1300

    In Process of creating a new responsive navbar

--- Automotive Repair WebApp/Views/Shared/mobile.js
+++ Automotive Repair WebApp/Views/Shared/mobile.js
@@ -1,12 +1,22 @@
-﻿const doc = document;
-const menuOpen = doc.querySelector(".menu");
-const menuClose = doc.querySelector(".close");
-const overlay = doc.querySelector(".overlay");
+﻿//const doc = document;
+//const menuOpen = doc.querySelector(".menu");
+//const menuClose = doc.querySelector(".close");
+//const overlay = doc.querySelector(".overlay");
 
-menuOpen.addEventListener("click", () => {
-    overlay.classList.add("overlay--active");
+//menuOpen.addEventListener("click", () => {
+//    overlay.classList.add("overlay--active");
+//});
+
+//menuClose.addEventListener("click", () => {
+//    overlay.classList.remove("overlay--active");
+//});
+
+
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
+
+navBarToggle.addEventListener("click", function () {
+    mainNav.classList.toggle("active");
 });
 
-menuClose.addEventListener("click", () => {
-    overlay.classList.remove("overlay--active");
-});
+
