24be99ceaa588fbf7e2445266f0cbd2b9b16f0f3
Aziz Patel <ac98844@avcol.school.nz>
2021-06-19 17:37:44 +1200

    Only Email domains ending with ".avondaletyres@gmail.com" will be accepted to register

--- Automotive Repair WebApp/ViewModels/RegisterViewModel.cs
+++ Automotive Repair WebApp/ViewModels/RegisterViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using Automotive_Repair_WebApp.Utilities;
 
 namespace Automotive_Repair_WebApp.ViewModels
 {
@@ -10,6 +11,8 @@ namespace Automotive_Repair_WebApp.ViewModels
     {
         [Required]
         [EmailAddress]
+        [ValidEmailDomain(allowedDomain: "avondaletyres@gmail.com",
+        ErrorMessage = "Email domain must be avondaletyres@gmail.com")]
         public string Email { get; set; }
 
         [Required]
