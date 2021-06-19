7b13339da3c53ebc5702f9e1d438451e208890e4
Aziz Patel <ac98844@avcol.school.nz>
2021-06-19 13:09:37 +1200

    Create Role Completed

---
+++ Automotive Repair WebApp/ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Automotive_Repair_WebApp.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        [Display(Name = "Role")]
+        public string RoleName { get; set; }
+    }
+}
